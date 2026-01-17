491115fbee930b9892e15708a3c69ba8cb659b7a
tbetti <tiana.bettinson@gmail.com>
2026-01-17 15:42:07 +0000

    changing nomenclature

--- src/services/helper_functions.py
+++ src/services/helper_functions.py
@@ -1,23 +1,23 @@
 def determine_taint(trades, target_builder):
-  net_size = 0
+  position = 0
   is_tainted = False
 
   for t in trades:
     trade_amount = float(t['sz'])
 
-    # add or subtract from net_size each trade
+    # add or subtract from position each trade
     if t['side'] == 'B': # buy
-      net_size += trade_amount
+      position += trade_amount
     elif t['side'] == 'A': # sell
-      net_size -= trade_amount
+      position -= trade_amount
 
     # check for differing builders
-    if net_size != 0 and t.get('builder') != target_builder:
+    if position != 0 and t.get('builder') != target_builder:
       is_tainted = True
 
     t['tainted'] = is_tainted
 
-    if net_size <= 0:
+    if position <= 0:
       is_tainted = False
   
   return trades
