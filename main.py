a7167744ccbb6972d0a19c0de69ee5866b11b6d0
tbetti <tiana.bettinson@gmail.com>
2026-01-17 13:02:01 +0000

    leaderboard should be running

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from src.api.leaderboard import router as leaderboard_router
 from src.api.trades import router as trades_router
 from src.api.pnl import router as pnl_rounter
 from src.api.positions import router as positions_router
 
 app = FastAPI(title="Hyperliquid Trading Challenge API")
 
+app.include_router(leaderboard_router)
 app.include_router(trades_router)
 app.include_router(pnl_rounter)
 app.include_router(positions_router)
